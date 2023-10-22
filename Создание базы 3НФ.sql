85d6b7822c8eebb3f80a94d20fe33c7045a8f4cf
VasyutinskayaDarya <vasyutinskaya.darya.study@gmail.com>
2023-10-22 17:55:22 +0300

    1st commit

---
+++ Создание базы 3НФ.sql
@@ -0,0 +1,13 @@
+-- Database: ski base
+
+-- DROP DATABASE IF EXISTS "ski base";
+
+CREATE DATABASE "ski base"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
